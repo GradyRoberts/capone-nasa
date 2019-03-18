edee0c673a0fdb4359de65e695d1e1c5050ca440
Grady Roberts <gnr7aj@virginia.edu>
2019-03-17 20:13:09 -0400

    Add URLs for advanced search and results

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('advanced-search/', views.advanced_search,
+         name='advanced_search'),
+    path('advanced-search/results/',
+         views.advanced_results, name='advanced_results'),
+    path('results/', views.results, name='results'),
 ]
